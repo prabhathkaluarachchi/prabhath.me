3c81dbc641d61cc473575f8f63d2702de19b93b8
prabhathkaluarachchi <fmprabhath@gmail.com>
2024-10-28 21:30:44 +0530

    mail updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -200,3 +200,46 @@
   });
 
 })();
+
+
+//this is for form submission styles
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the form element
+  var form = document.querySelector('.contact-form');
+
+  // Add event listener for form submission
+  form.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevent the default form submission
+
+      // Send the form data using Fetch API or any other method
+      fetch(form.action, {
+          method: form.method,
+          body: new FormData(form)
+      })
+          .then(function (response) {
+              if (response.ok) {
+                  // If the response is successful, show the success message
+                  var successMessage = document.createElement('div');
+                  successMessage.textContent = 'Message sent Successfully';
+                  successMessage.style.backgroundColor = '#4CAF50'; // Green background color
+                  successMessage.style.color = '#FFF'; // White text color
+                  successMessage.style.padding = '10px'; // Padding
+                  successMessage.style.marginBottom = '20px'; // Margin bottom
+                  successMessage.style.borderRadius = '5px'; // Border radius
+                  successMessage.style.textAlign = 'center'; // Text alignment
+                  successMessage.style.marginTop = '10px'; // Add margin to the top
+                  form.parentNode.insertBefore(successMessage, form.nextSibling);
+
+                  // Optionally, you can clear the form fields after successful submission
+                  form.reset();
+              } else {
+                  // If there's an error in form submission, show an error message
+                  alert('Error: Message was not sent');
+              }
+          })
+          .catch(function (error) {
+              console.error('Error:', error);
+              alert('Error: Message was not sent');
+          });
+  });
+});
